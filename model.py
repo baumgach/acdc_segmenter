fixed batch normalization

model.py
@@ -48,7 +48,7 @@ def loss(logits, labels, weight_decay=0.00005):
 
 def predict(images, inference_handle):
 
-    logits = inference_handle(images, training=False)
+    logits = inference_handle(images, training=tf.constant(False, dtype=tf.bool))
     softmax = tf.nn.softmax(logits)
     mask = tf.arg_max(logits, dimension=3)
 
@@ -62,10 +62,6 @@ def training(loss, optimizer_handle, learning_rate, **kwargs):
         optimizer = optimizer_handle(learning_rate=learning_rate, momentum=momentum)
     else:
         optimizer = optimizer_handle(learning_rate=learning_rate)
-
-    #optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    # optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
-    # optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 
     # Create a variable to track the global step.
     global_step = tf.Variable(0, name='global_step', trainable=False)

tfwrapper/layers.py
@@ -178,9 +178,37 @@ def conv2D_dilated_layer(bottom,
 
 ### BATCH_NORM SHORTCUTS #####################################################################################
 
+
 def batch_normalisation_layer(bottom, name, training):
-    # TODO: This one is a bit pointless: Either add variable summaries or remove
-    return tf.layers.batch_normalization(bottom, name=name, training=training)
+    """
+    Batch normalization on feedforward maps. (Adapted from https://github.com/tensorflow/tensorflow/issues/1122)
+    Args:
+        bottom:      input from last layer
+        name:        name of layer
+        training:    boolean tf.Variable, true indicates training phase (Note: cannot be a simple Python bool)
+    Return:
+        normed:      batch-normalized maps
+    """
+    with tf.variable_scope(name):
+
+        n_out = bottom.get_shape().as_list()[-1]
+
+        init_beta = tf.constant(0.0, shape=[n_out], dtype=tf.float32)
+        init_gamma = tf.constant(1.0, shape=[n_out],dtype=tf.float32)
+        beta = tf.get_variable(name=name+'_beta', dtype=tf.float32, initializer=init_beta, regularizer=None, trainable=True)
+        gamma = tf.get_variable(name=name+'_gamma', dtype=tf.float32, initializer=init_gamma, regularizer=None, trainable=True)
+        batch_mean, batch_var = tf.nn.moments(bottom, [0], name=name+'_moments')
+        ema = tf.train.ExponentialMovingAverage(decay=0.5)
+
+        def mean_var_with_update():
+            ema_apply_op = ema.apply([batch_mean, batch_var])
+            with tf.control_dependencies([ema_apply_op]):
+                return tf.identity(batch_mean), tf.identity(batch_var)
+
+        mean, var = tf.cond(training, mean_var_with_update, lambda: (ema.average(batch_mean), ema.average(batch_var)))
+        normed = tf.nn.batch_normalization(bottom, mean, var, beta, gamma, 1e-3)
+
+    return normed
 
 def conv2D_layer_bn(bottom,
                     name,

train.py
@@ -22,20 +22,21 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 PROJECT_ROOT = '/scratch_net/bmicdl03/code/python/ACDC_challenge_refactored'
 # LOG_DIR = os.path.join(PROJECT_ROOT, 'acdc_logdir/lisa_net_deeper_adam_sched_reg0.00005_lr0.001_aug')
-# LOG_DIR = os.path.join(PROJECT_ROOT, 'acdc_logdir/lisa_net_deeper_sgd_sched_reg0.00005_lr0.1_aug_bn2')
+LOG_DIR = os.path.join(PROJECT_ROOT, 'acdc_logdir/lisa_net_deeper_adam_sched_reg0.00005_lr0.1_aug_newbn')
 # LOG_DIR = os.path.join(PROJECT_ROOT, 'acdc_logdir/ln_4pool_adam_reg0.00005_lr0.001_aug')
-LOG_DIR = os.path.join(PROJECT_ROOT, 'acdc_logdir/ln_3pool_stackconvs_adam_reg0.00005_lr0.001_aug2')
+# LOG_DIR = os.path.join(PROJECT_ROOT, 'acdc_logdir/ln_3pool_stackconvs_adam_reg0.00005_lr0.001_aug2')
+# LOG_DIR = os.path.join(PROJECT_ROOT, 'acdc_logdir/debug')
 
-BATCH_SIZE = 32
-LEARNING_RATE = 0.001
+BATCH_SIZE = 20
+LEARNING_RATE = 0.1
 DATA_FILE = 'data_288x288.hdf5'  #'data_288x288_plusregs.hdf5'
-# MODEL_HANDLE = model_zoo.lisa_net_deeper_bn
+MODEL_HANDLE = model_zoo.lisa_net_deeper_bn
 # MODEL_HANDLE = model_zoo.lisa_net_one_more_pool
-MODEL_HANDLE = model_zoo.lisa_net_3pool_stack_convs
+# MODEL_HANDLE = model_zoo.lisa_net_3pool_stack_convs
 OPTIMIZER_HANDLE = tf.train.AdamOptimizer
 # OPTIMIZER_HANDLE = tf.train.GradientDescentOptimizer
 SCHEDULE_LR = True
-WARMUP_TRAINING = False
+WARMUP_TRAINING = True
 AUGMENT_BATCH = True
 WEIGHT_DECAY = 0.00005
 
@@ -354,6 +355,10 @@ def run_training():
                         logging.warning('Reducing learning rate!')
                         curr_lr /= 10.0
                         logging.info('Learning rate changed to: %f' % curr_lr)
+
+                        # reset loss history to give the optimisation some time to start decreasing again
+                        loss_gradient = np.inf
+                        loss_history = []
 
                     if val_loss <= best_val:
 