4df7802c5f150f91beff25ed678edcaaea2ceff5
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2019-02-23 14:15:29 +0100

    Modified requirements

--- config/system.py
+++ config/system.py
@@ -13,10 +13,10 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 # when it is executed on one of the clusters.
 
 at_biwi = False  # Are you running this code from the ETH Computer Vision Lab (Biwi)?
-project_root = '/scratch_net/bmicdl03/code/python/acdc_segmenter_public'
-data_root = '/scratch_net/bmicdl03/data/ACDC_challenge_20170617/'
-test_data_root = '/scratch_net/bmicdl03/data/ACDC_challenge_testdata/'
-local_hostnames = ['bmicdl03']  # used to check if on cluster or not,
+project_root = '../'
+data_root = '../data/training'
+test_data_root = '../data/testing'
+local_hostnames = ['frankenstein']  # used to check if on cluster or not,
                                 # enter the name of your local machine
 
 ##################################################################################
--- train.py
+++ train.py
@@ -24,8 +24,8 @@ import acdc_data
 # from experiments import unet2D_bn_modified_dice as exp_config
 # from experiments import unet2D_bn_modified_wxent as exp_config
 # from experiments import unet2D_bn_modified_xent as exp_config
-# from experiments import unet2D_bn_wxent as exp_config
-from experiments import unet3D_bn_modified_wxent as exp_config
+from experiments import unet2D_bn_wxent as exp_config
+# from experiments import unet3D_bn_modified_wxent as exp_config
 
 ########################################################################################
 
