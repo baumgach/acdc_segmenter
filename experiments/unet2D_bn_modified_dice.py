5dabac3a4c625e6e7a117591a9618a9da8586a39
baumgach <c.f.baumgartner@gmail.com>
2019-06-04 14:51:35 +0200

    reset settings

--- experiments/unet2D_bn_modified_dice.py
+++ experiments/unet2D_bn_modified_dice.py
@@ -13,7 +13,7 @@ target_resolution = (1.36719, 1.36719)
 nlabels = 4
 
 # Training settings
-batch_size = 4
+batch_size = 10
 learning_rate = 0.01
 optimizer_handle = tf.train.AdamOptimizer
 schedule_lr = False
--- experiments/unet2D_bn_modified_wxent.py
+++ experiments/unet2D_bn_modified_wxent.py
@@ -1,7 +1,7 @@
 import model_zoo
 import tensorflow as tf
 
-experiment_name = 'unet2D_bn_modified_wxent_bn'
+experiment_name = 'unet2D_bn_modified_wxent_bn_test'
 
 # Model settings
 model_handle = model_zoo.unet2D_bn_modified
